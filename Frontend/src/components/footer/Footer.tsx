a699c61827b06d349d168c8ff1f5145cbef8a700
vietvanba <baviet19@gmail.com>
2024-04-05 18:33:11 +0700

    edit footer

--- Frontend/src/components/footer/Footer.tsx
+++ Frontend/src/components/footer/Footer.tsx
@@ -2,9 +2,12 @@ import "./footer.scss";
 export const Footer = () => {
   return (
     <div className="footer">
-      <div className="text">© 2024 Viet Van Ba</div>
       <div className="text">
-        About this website: built with React, Nginx, Framer Motion, Spring Boot
+        <a href="https://www.linkedin.com/in/vietvanba/">© 2024 Viet Van Ba</a>
+      </div>
+      <div className="text">
+        About this website: built with React, Nginx, Framer Motion, Spring Boot,
+        Docker
       </div>
     </div>
   );
