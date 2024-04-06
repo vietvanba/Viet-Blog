f04f1fa1287937651c712674a5eb51f38c8033fb
vietvanba <baviet19@gmail.com>
2024-04-06 09:00:26 +0700

    fix bug No validator could be found for @NotBlank validating type Date

--- Backend/authservice/src/main/java/com/blog/authservice/payload/RegisterRequest.java
+++ Backend/authservice/src/main/java/com/blog/authservice/payload/RegisterRequest.java
@@ -3,6 +3,7 @@ package com.blog.authservice.payload;
 import com.blog.authservice.enums.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,7 +24,7 @@ public class RegisterRequest {
     private String firstName;
     @NotBlank(message = "Please fill last name")
     private String lastName;
-    @NotBlank(message = "Please select birthday")
+    @NotNull(message = "Please select birthday")
     private Date birthday;
     @NotBlank(message = "Please fill your email")
     @Email(message = "Please correct your email")
