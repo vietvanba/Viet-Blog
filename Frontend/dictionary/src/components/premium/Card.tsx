96999b3edfc747f6ab64641f4fcff901c7e97bee
vietvanba <baviet19@gmail.com>
2024-06-18 22:28:48 +0700

    modify code for premium plan

--- Frontend/dictionary/src/components/premium/Card.tsx
+++ Frontend/dictionary/src/components/premium/Card.tsx
@@ -1,5 +1,6 @@
 import { FaCheck } from "react-icons/fa";
 type PremiumPlan = {
+  id: string;
   name: string;
   price: string;
   period: string;
@@ -12,20 +13,32 @@ type CardProps = {
 };
 export const Card: React.FC<CardProps> = (props) => {
   return (
-    <div className="border p-5 w-96 flex flex-col justify-center gap-5 border-neutral-900 rounded-xl">
-      <div className="font-bold ">{props.plan.name}</div>
-      <div className="font-extralight text-xs">{props.plan.introduction}</div>
-      <div className=" flex flex-row items-end">
-        <div className="font-bold text-4xl">{props.plan.price}</div>
-        <div className="">/{props.plan.period}</div>
-      </div>
-      {props.plan.benefits.map((b) => (
-        <div className="flex flex-row gap-2 items-center">
-          <FaCheck />
-          <div className="font-extralight">{b}</div>
+    <div className="border p-5 w-96 flex flex-col justify-between gap-5 border-neutral-900 dark:border-neutral-100 rounded-xl ">
+      <div className="flex flex-col gap-5">
+        <div className="font-bold text-3xl text-neutral-950 dark:text-neutral-100">
+          {props.plan.name}
         </div>
-      ))}
-      <div className="w-full text-center cursor-pointer border border-neutral-900 rounded-xl p-1">Choose plan</div>
+        <div className="font-extralight text-xs text-neutral-950 dark:text-neutral-100">
+          {props.plan.introduction}
+        </div>
+        <div className="flex flex-row items-end">
+          <div className="font-bold text-4xl text-neutral-950 dark:text-neutral-100">
+            {props.plan.price}
+          </div>
+          <div className="text-neutral-950 dark:text-neutral-100">
+            /{props.plan.period}
+          </div>
+        </div>
+        {props.plan.benefits.map((b) => (
+          <div className="flex flex-row gap-2 items-center text-neutral-950 dark:text-neutral-100">
+            <FaCheck />
+            <div className="font-extralight">{b}</div>
+          </div>
+        ))}
+      </div>
+      <div className="w-full text-center cursor-pointer border-neutral-950 dark:border-neutral-100 border rounded-xl p-1 bg-neutral-950 dark:bg-neutral-100 dark:text-neutral-950 text-neutral-100 hover:dark:bg-neutral-950 hover:bg-neutral-100 hover:text-neutral-950 hover:dark:text-neutral-100">
+        Choose plan
+      </div>
     </div>
   );
 };
--- Frontend/dictionary/src/pages/premium/Premium.tsx
+++ Frontend/dictionary/src/pages/premium/Premium.tsx
@@ -2,11 +2,19 @@ import { Card } from "../../components/premium/Card";
 import plan from "./plan.json";
 export const Premium = () => {
   return (
-    <div className="row-span-9 overflow-scroll">
+    <div className="row-span-9 overflow-scroll hide-scrollbar">
+      <div className="flex justify-center dark:text-neutral-100 text-neutral-950 text-3xl text-wrap py-2">
+        Welcome to the Premium Plans page!
+      </div>
+      <div className="flex justify-center dark:text-neutral-100 text-neutral-950 text-xl text-wrap pt-2 pb-4">
+        Enhance your language learning experience with our tailored subscription
+        options.
+      </div>
+
       <div className="flex flex-row justify-center gap-5 flex-wrap ">
-        <Card plan={plan} />
-        <Card plan={plan} />
-        <Card plan={plan} />
+        {plan.map((x) => (
+          <Card plan={x} />
+        ))}
       </div>
     </div>
   );
